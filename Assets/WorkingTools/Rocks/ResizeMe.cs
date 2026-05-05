fd22e451075f8f949f3317a167b1b51dd91941e4
Josh Z <jbot237@gmail.com>
2026-05-05 14:20:43 -0400

    Added 6 new rocks. Made Rocks re-sizeable, and added remove, gravity, and redo buttons ( plus a Help window )

---
+++ Assets/WorkingTools/Rocks/ResizeMe.cs
@@ -0,0 +1,112 @@
+using LearnXR.Core.Utilities;
+using Oculus.Interaction;
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using static UnityEngine.UI.GridLayoutGroup;
+
+public class ResizeMe : MonoBehaviour
+{
+
+    public float minimumSize; // the minimum size we want the object to be. Defaults to the creation size.
+    private float startingDistance; // What the starting distance between controllers prior to resizing
+    private Vector3 startingSizeOfResize; // What the size of the gameobject is prior to resizing
+    public float resizeSpeed = 2.0f;
+    private Grabbable grabbable;
+    private int isBeingHeld = 0; // number of hands holding the thing
+    //public bool UseGravity;
+    //private Rigidbody myRigidbody;
+
+    void Awake()
+    {
+        minimumSize = transform.localScale.x;
+
+        grabbable = GetComponentInChildren<Grabbable>();
+
+        if (grabbable == null)
+        {
+            throw new InvalidOperationException("Object requires a Grabbable component");
+        }
+
+        grabbable.WhenPointerEventRaised += OnPointerEventRaised;
+    }
+
+    private void OnDestroy()
+    {
+        if (grabbable != null)
+        {
+            grabbable.WhenPointerEventRaised -= OnPointerEventRaised;
+        }
+    }
+
+    private void OnPointerEventRaised(PointerEvent pointerEvent)
+    {
+        if (isBeingHeld == 2 && pointerEvent.Type == PointerEventType.Move) {
+            //print("Resizing");
+            resize();
+            return;
+        }
+        //print("Something is Happening");
+        if (grabbable == null || pointerEvent.Type == PointerEventType.Unselect)
+        {
+            isBeingHeld--;
+            return;
+        }
+        
+
+        if (pointerEvent.Type == PointerEventType.Select)
+        {
+            isBeingHeld++;
+            if (grabbable.SelectingPointsCount == 2)
+            {
+                startingDistance = getControlerDistances();
+                startingSizeOfResize = transform.localScale;
+            }
+            return;
+        }
+        //print($"Pointer Type: {pointerEvent.Type}");
+        //print($"PointerEvent: {pointerEvent}");
+
+
+        //if (isBeingHeld == 2) { 
+        //    print("Hello! Im Being Double Grabbed!");
+        //}
+
+        //if (grabbable.SelectingPointsCount == 2 && isBeingHeld != 0)
+        //{
+        //    print($"Grab amount: {grabbable.SelectingPointsCount}");
+        //    return;
+        //}
+
+    }
+
+    void resize()
+    {
+        float sizeAmount = getControlerDistances();
+        sizeAmount -= startingDistance;
+        sizeAmount *= resizeSpeed;
+
+        transform.localScale = startingSizeOfResize+ new Vector3(sizeAmount,sizeAmount,sizeAmount);
+        
+        if (transform.localScale.x < minimumSize) { 
+            transform.localScale = new Vector3(minimumSize, minimumSize, minimumSize);
+        }
+
+        //print($"ResizeAmount: {sizeAmount} / {transform.localScale}");
+    }
+
+    float getControlerDistances()
+    {
+        Vector3 hand1 = grabbable.GrabPoints[0].position;
+        Vector3 hand2 = grabbable.GrabPoints[1].position;
+
+        return (Vector3.Distance(hand1, hand2));
+    }
+
+
+    public void print(string message)
+    {
+        Debug.Log(message);
+        SpatialLogger.Instance.LogInfo(message);
+    }
+}
--- Assets/WorkingTools/Rocks/RockMenu.cs
+++ Assets/WorkingTools/Rocks/RockMenu.cs
@@ -1,16 +1,284 @@
+using PurrNet;
+using System;
+using System.IO;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class RockMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private const float SpawnDistanceFromCamera = 0.75f;
+    public OVRCameraRig _cameraRig;
+    private Canvas _canvas;
+
+    [Tooltip("Helper for ray interactions")]
+    public OVRRayHelper RayHelper;
+    [Tooltip("Input module for handling VR input")]
+    public OVRInputModule InputModule;
+    [Tooltip("Raycaster for handling ray interactions")]
+    public OVRRaycaster Raycaster;
+    [Tooltip("Gaze pointer for VR interactions")]
+    public OVRGazePointer GazePointer;
+
+    public GameObject holdMyRocks;
+    public PlayerID player;
+
+    public GameObject Log;
+    public GameObject UsageBox;
+
+    private Action<GameObject> lastCommand;
+    private GameObject lastGameObj;
+    public int rockState = 0; // 0 is kinematic, 1 is not kinematic (asteroids), and 2 is with gravity
+
+
+    //public GameObject rockButtonHolder;
+    //public GameObject rockButtonTemp;
+    //public string jsonFile;
+
+    private void Awake()
     {
-        
+        //StartCoroutine();
+        _canvas = GetComponent<Canvas>();
+        SnapMenuInFrontOfCamera();
+        SetupInteractionDependencies();
+        //    string folderPath = Application.dataPath + "/WorkingTools/Rocks";
+
+        //    if (Directory.Exists(folderPath))
+        //    {
+        //        //string[] files = Directory.GetFiles(folderPath, "*.*", SearchOption.TopDirectoryOnly); // Fetch all files
+        //        string[] folders = Directory.GetDirectories(folderPath, "*", SearchOption.TopDirectoryOnly); // Fetch all folders
+        //        foreach (string folder in folders)
+        //        {
+        //            Debug.Log("Folder: " + folder);
+        //            Resources.Load(folder); // If we do this method you need to put everything inside of the Resources folder
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Debug.LogError("Folder does not exist: " + folderPath);
+        //    }
     }
 
-    // Update is called once per frame
+    //private void CreateButton(string folderName,Sprite image)
+    //{
+    //    GameObject newButton = Instantiate(rockButtonTemp);
+    //    newButton.name = folderName;
+
+    //    TMP_Text text = newButton.GetComponentInChildren<TMPro.TMP_Text>();
+    //    text.text = folderName.Substring(0,1).ToUpper() + folderName.Substring(1); // make the folder name capital
+
+    //    newButton.GetComponentInChildren<Image>().sprite = image;
+
+    //}
+
+
+
     void Update()
     {
+        if (OVRInput.GetDown(OVRInput.RawButton.Start))
+        {
+            ToggleMenu();
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.A))
+        {
+            lastCommand.Invoke(lastGameObj);
+        }
+
+
+        if (OVRInput.GetDown(OVRInput.RawButton.B))
+        {
+            ChangeAllRockStates(holdMyRocks);
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.X))
+        {
+            DestroyAllMyRocks(holdMyRocks);
+        }
+        if (OVRInput.GetDown(OVRInput.RawButton.Y))
+        {
+            ToggleLogAndHelp(null);
+        }
         
+
+        Billboard();
+    }
+
+    public void CreateRock(GameObject rock)
+    {
+        GameObject newRock = Instantiate(rock);
+        newRock.transform.SetParent(holdMyRocks.transform);
+        newRock.transform.position = _cameraRig.rightControllerAnchor.gameObject.transform.position;
+        newRock.transform.rotation = _cameraRig.rightControllerAnchor.gameObject.transform.rotation;
+        ChangeRockState(newRock);
+
+        if (lastCommand != CreateRock || lastGameObj != rock)
+        {
+            lastCommand = CreateRock;
+            lastGameObj = rock;
+        }
+    }
+
+    public void DestroyAllMyRocks(GameObject rockHolder)
+    {
+        while (rockHolder.transform.childCount > 0)
+        {
+            DestroyImmediate(rockHolder.transform.GetChild(0).gameObject);
+        }
+        if (lastCommand != DestroyAllMyRocks || lastGameObj != rockHolder)
+        {
+            lastCommand = DestroyAllMyRocks;
+            lastGameObj = rockHolder;
+        }
+    }
+
+    public void ChangeAllRockStates(GameObject rockHolder)
+    {
+        rockState++;
+        rockState = rockState % 3; // loop back to 0
+
+        foreach (Transform rock in rockHolder.transform)
+        {
+            ChangeRockState(rock.gameObject);
+        }
+
+        if (lastCommand != ChangeAllRockStates || lastGameObj != rockHolder)
+        {
+            lastCommand = ChangeAllRockStates;
+            lastGameObj = rockHolder;
+        }
+    }
+
+    public void ChangeRockState(GameObject rock)
+    {
+        Rigidbody rockBody = rock.GetComponentInChildren<Rigidbody>();
+        rockBody.isKinematic = rockState<1; // 0 is kin
+        // 1 is no grav no kin (asteroids)
+        rockBody.useGravity = rockState == 2; // 2 is grav
+
+    }
+
+
+
+
+
+    void ToggleLogAndHelp(GameObject Dummy)
+    {
+        Log.SetActive(!Log.activeInHierarchy);
+        UsageBox.SetActive(!UsageBox.activeInHierarchy);
+
+        if (lastCommand != ToggleLogAndHelp || lastGameObj != null)
+        {
+            lastCommand = ToggleLogAndHelp;
+            lastGameObj = null;
+        }
+    }
+
+
+    // Makes the menu look at you
+    private void Billboard()
+    {
+        if (!_canvas)
+        {
+            return;
+        }
+
+        var direction = _canvas.transform.position - _cameraRig.centerEyeAnchor.transform.position;
+        if (direction.sqrMagnitude > 0.01f)
+        {
+            var rotation = Quaternion.LookRotation(direction);
+            _canvas.transform.rotation = rotation;
+        }
+    }
+
+    private void ToggleMenu()
+    {
+        _canvas.enabled = !_canvas.enabled;
+        if (_canvas.enabled)
+            SnapMenuInFrontOfCamera();
+    }
+
+    private void SnapMenuInFrontOfCamera()
+    {
+        transform.position = _cameraRig.centerEyeAnchor.transform.position +
+                             _cameraRig.centerEyeAnchor.transform.forward * SpawnDistanceFromCamera;
+    }
+
+    // All the stuff for the controls
+    private void SetupInteractionDependencies()
+    {
+        if (!_cameraRig)
+        {
+            return;
+        }
+
+        GazePointer.rayTransform = _cameraRig.centerEyeAnchor;
+        InputModule.rayTransform = _cameraRig.rightControllerAnchor;
+        Raycaster.pointer = _cameraRig.rightControllerAnchor.gameObject;
+        if (_cameraRig.GetComponentsInChildren<OVRRayHelper>(false).Length > 0)
+        {
+            return;
+        }
+
+        var rightControllerHelper =
+            _cameraRig.rightControllerAnchor.GetComponentInChildren<OVRControllerHelper>();
+        if (rightControllerHelper)
+        {
+            rightControllerHelper.RayHelper =
+                Instantiate(RayHelper, Vector3.zero, Quaternion.identity, rightControllerHelper.transform);
+            rightControllerHelper.RayHelper.gameObject.SetActive(true);
+        }
+
+        var leftControllerHelper =
+            _cameraRig.leftControllerAnchor.GetComponentInChildren<OVRControllerHelper>();
+        if (leftControllerHelper)
+        {
+            leftControllerHelper.RayHelper =
+                Instantiate(RayHelper, Vector3.zero, Quaternion.identity, leftControllerHelper.transform);
+            leftControllerHelper.RayHelper.gameObject.SetActive(true);
+        }
+
+        var hands = _cameraRig.GetComponentsInChildren<OVRHand>();
+        foreach (var hand in hands)
+        {
+            hand.RayHelper =
+                Instantiate(RayHelper, Vector3.zero, Quaternion.identity, _cameraRig.trackingSpace);
+            hand.RayHelper.gameObject.SetActive(true);
+        }
+    }
+
+    private Ray GetControllerRay()
+    {
+        Vector3 rayOrigin;
+        Vector3 rayDirection;
+        if (OVRInput.activeControllerType == OVRInput.Controller.Touch
+            || OVRInput.activeControllerType == OVRInput.Controller.RTouch)
+        {
+            rayOrigin = _cameraRig.rightHandOnControllerAnchor.position;
+            rayDirection = _cameraRig.rightHandOnControllerAnchor.forward;
+        }
+        else if (OVRInput.activeControllerType == OVRInput.Controller.LTouch)
+        {
+            rayOrigin = _cameraRig.leftHandOnControllerAnchor.position;
+            rayDirection = _cameraRig.leftHandOnControllerAnchor.forward;
+        }
+        else // hands
+        {
+            var rightHand = _cameraRig.rightHandAnchor.GetComponentInChildren<OVRHand>();
+            // can be null if running in Editor with Meta Linq app and the headset is put off
+            if (rightHand != null)
+            {
+                rayOrigin = rightHand.PointerPose.position;
+                rayDirection = rightHand.PointerPose.forward;
+            }
+            else
+            {
+                rayOrigin = _cameraRig.centerEyeAnchor.position;
+                rayDirection = _cameraRig.centerEyeAnchor.forward;
+            }
+        }
+
+        return new Ray(rayOrigin, rayDirection);
     }
 }
